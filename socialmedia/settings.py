9d0e282c177a173c02e236df9638b961ad037036
Prakhyat Wadhwa <prakhyatwadhwa7403@gmail.com>
2025-04-26 04:35:59 +0530

    hosts changed

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['socialmedia.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
